6c2f9b7e9fb9817f57edec95f76e866b8e6f294e
zhouapples@126.com <zhouapples@126.com>
2020-11-21 15:22:16 +0800

    first commit

---
+++ src/main/java/com/meeting/appo/AppoApplication.java
@@ -0,0 +1,13 @@
+package com.meeting.appo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meeting/appo/AppoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meeting.appo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
