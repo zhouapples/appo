69d07b4d5f6b0d1916bd7e41c583016014bef034
zhouapples <zhouapples@126.com>
2020-12-12 12:07:31 +0800

    mybatis crud test success

---
+++ src/test/java/com/meeting/appo/GetStatrusTest.java
@@ -0,0 +1,73 @@
+package com.meeting.appo;
+
+import com.meeting.appo.dao.StatusMapper;
+import com.meeting.appo.entities.Status;
+import com.meeting.appo.entities.User;
+import com.meeting.appo.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GetStatrusTest {
+
+    @Test
+    public void showAll(){
+        //1.获取sqlsession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //2.获取mapper
+        StatusMapper mapper = sqlSession.getMapper(StatusMapper.class);
+
+        List<Status> statusList = mapper.getStatusList();
+        for (Status s:statusList) {
+            System.out.println(s);
+        }
+        //3.关闭SQLSession
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void addOneUser(){
+        //1.获取sqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StatusMapper mapper = sqlSession.getMapper(StatusMapper.class);
+        mapper.addUser(new User(3,"蟹老板","13388639451","物业","2012/10/22 12:25:59",false));
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void getStatusByUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StatusMapper mapper = sqlSession.getMapper(StatusMapper.class);
+        Status status = mapper.getStatusByUser(1);
+        System.out.println(status);
+    }
+
+    @Test
+    public void updateUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StatusMapper mapper = sqlSession.getMapper(StatusMapper.class);
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("myName","章鱼哥");
+        map.put("uid",1);
+        mapper.updateUser(map);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StatusMapper mapper = sqlSession.getMapper(StatusMapper.class);
+        mapper.deleteUser(3);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+
+}
